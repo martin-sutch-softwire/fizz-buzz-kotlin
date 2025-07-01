43bdba7ea001daf9de9502ec01d91158dd78b31c
martin-sutch-softwire <martin.sutch@softwire.com>
2025-07-01 10:12:18 +0100

    1 to 100, Fizz on 3 and Buzz on 5

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,23 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    var num = 1
+    while (num <= 100 ) {
+        var ln = ""
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+        if (num % 3 == 0) {
+            ln += "Fizz"
+        }
+
+        if (num % 5 == 0) {
+            ln += "Buzz"
+        }
+
+        if (ln == "") {
+            ln = num.toString()
+        }
+
+        println(ln)
+        num++
     }
 }
