cc646b4c5ba38c08c368299c520e5a06d91a236b
martin-sutch-softwire <martin.sutch@softwire.com>
2025-07-01 11:01:40 +0100

    Refactor

--- src/Main.kt
+++ src/Main.kt
@@ -1,23 +1,10 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    var num = 1
-    while (num <= 100 ) {
-        var ln = ""
-
-        if (num % 3 == 0) {
-            ln += "Fizz"
+    for (num in 1 .. 100) {
+        var output = buildString {
+            if (num % 3 == 0) append("Fizz")
+            if (num % 5 == 0) append("Buzz")
         }
-
-        if (num % 5 == 0) {
-            ln += "Buzz"
-        }
-
-        if (ln == "") {
-            ln = num.toString()
-        }
-
-        println(ln)
-        num++
+        if (output == "") output = num.toString()
+        println(output)
     }
 }
