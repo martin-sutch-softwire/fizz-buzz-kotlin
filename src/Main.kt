cc0c7fa3d597f546aa80876530cc7a563b7a152d
martin-sutch-softwire <martin.sutch@softwire.com>
2025-07-01 16:11:11 +0100

    User selection of 6 rules via command line and upper limit of output

--- src/Main.kt
+++ src/Main.kt
@@ -1,22 +1,66 @@
 fun main() {
-    for (num in 1 .. 255) {
+    inputRules()
+}
+
+data class Rule(
+    val description: String,
+    var isEnabled: Boolean,
+)
+
+val rules = mutableMapOf(
+    "3" to Rule("Fizz every 3rd number.", true),
+    "5" to Rule("Buzz every 5th number.", true),
+    "7" to Rule("Bang every 7th number.", true),
+    "11" to Rule("Bong every 11th number, replacing any Fizz/Buzz/Bang.", true),
+    "13" to Rule("Fezz every 13th number, before the first B-word (otherwise at end).", true),
+    "17" to Rule("Every 17th number reverse the order of the words.", true)
+)
+
+fun toggleRule(ruleKey: String) {
+    rules[ruleKey]?.let {
+        it.isEnabled = !it.isEnabled
+    }
+}
+
+fun inputRules() {
+    println("RULES:")
+    for ((key, rule) in rules) {
+        println("${if (rule.isEnabled) "\uD83D\uDDF9 | " else "☐ | "}Type '${key}' to ${if (rule.isEnabled) "remove" else "add"} rule: ${rule.description}")
+    }
+    println("Then press enter to continue.")
+    val ruleInput = readln().toIntOrNull()
+    if (ruleInput is Int) {
+        toggleRule(ruleInput.toString())
+        inputRules()
+    } else inputLimit()
+}
+
+fun inputLimit() {
+    println("And how far are we going? Give us a number...")
+    val limit = readln().toIntOrNull()
+    if (limit is Int) fizzBuzz(limit) else inputLimit()
+}
+
+fun fizzBuzz(limit: Int) {
+    for (num in 1..limit) {
         var output =
             buildString {
-                if (num % 11 == 0) append("Bong") else {
-                    if (num % 3 == 0) append("Fizz")
-                    if (num % 5 == 0) append("Buzz")
-                    if (num % 7 == 0) append("Bang")
+                if (rules["11"]!!.isEnabled && num % 11 == 0) append("Bong") else {
+                    if (rules["3"]!!.isEnabled && num % 3 == 0) append("Fizz")
+                    if (rules["5"]!!.isEnabled && num % 5 == 0) append("Buzz")
+                    if (rules["7"]!!.isEnabled && num % 7 == 0) append("Bang")
                 }
-                if (num % 13 == 0) {
+                if (rules["13"]!!.isEnabled && num % 13 == 0) {
                     val index = indexOf("B").takeIf { it != -1 } ?: count()
                     insert(index, "Fezz")
                 }
             }
-        if (num % 17 == 0) {
+        if (rules["17"]!!.isEnabled && num % 17 == 0) {
             val words = output.split("(?=[A-Z])".toRegex())
             output = words.reversed().joinToString("")
-            }
+        }
         if (output == "") output = num.toString()
         println(output)
     }
+    inputRules()
 }
